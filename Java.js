c8e91b808a2365634e72adfb678dc14715042699
Aiviatrist <92304985+Aiviatrist@users.noreply.github.com>
2022-02-17 20:04:29 +1000

    Add files via upload

--- Java.js
+++ Java.js
@@ -11,3 +11,18 @@ function lolcap(){
     document.getElementById("result").innerHTML='<img id="hi" src="'+data_uri+'" >';
     });
 }
+var classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/icOZYGEOs/model.json",modelloaded);
+function loliden(){
+        img=document.getElementById('hmmm');
+        classifier.classify(img,gotresult);
+    function gotresult(error,results){
+        if(error){
+        console.error(error);
+        }
+        else{
+            console.log(results);
+            document.getElementById("result_object_name").innerHTML=results[0].label;
+            document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
+        }
+    }
+}
